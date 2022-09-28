d7c0755726b5339b43a8bfdeca1d16ed56bdc864
Sylvain Kuchen <sylvain.kuchen@sonarsource.com>
2022-09-28 18:28:47 +0200

    Add multiple test cases

--- regex-parsing/src/test/java/org/sonarsource/analyzer/commons/regex/helpers/SatisfiabilityCheckerTest.java
+++ regex-parsing/src/test/java/org/sonarsource/analyzer/commons/regex/helpers/SatisfiabilityCheckerTest.java
@@ -25,7 +25,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.sonarsource.analyzer.commons.regex.RegexParseResult;
 import org.sonarsource.analyzer.commons.regex.ast.FlagSet;
-import org.sonarsource.analyzer.commons.regex.helpers.smt.SatisfiabilityChecker;
+import org.sonarsource.analyzer.commons.regex.smt.SatisfiabilityChecker;
 import org.sosy_lab.common.configuration.InvalidConfigurationException;
 import org.sosy_lab.java_smt.SolverContextFactory;
 import org.sosy_lab.java_smt.api.SolverContext;
@@ -60,6 +60,7 @@ class SatisfiabilityCheckerTest {
   void testSimpleSat() {
     assertSatisfiable("a").isTrue();
     assertSatisfiable("[a-z]").isTrue();
+    assertSatisfiable("[0-9]").isTrue();
     assertSatisfiable("a|b").isTrue();
     assertSatisfiable("^a$").isTrue();
     assertSatisfiable("a*").isTrue();
@@ -67,31 +68,105 @@ class SatisfiabilityCheckerTest {
     assertSatisfiable("a++").isTrue();
     assertSatisfiable("abc").isTrue();
     assertSatisfiable("abc(d|e)").isTrue();
-    assertSatisfiable("(?:a(?!bc)|d)+bc").isTrue();
-    assertSatisfiable("(?:a((?!bc)|d)*)+bc").isTrue();
-    assertSatisfiable("a(?!:abc):ab").isTrue();
-    assertSatisfiable("(?:-(?:one|[0-9]+([a-z](?=[^a-z]|$)|st|nd|rd|th)?))*").isTrue();
-    assertSatisfiable("(..(?=ab))*").isTrue();
-    assertSatisfiable("(?=a)a").isTrue();
-    assertSatisfiable("(?=a)..").isTrue();
-    assertSatisfiable("(?=a)ab").isTrue();
-    assertSatisfiable("(?!ab)..").isTrue();
-    assertSatisfiable("(?<=a)b").isTrue();
-    assertSatisfiable("a(?=b)").isTrue();
-    assertSatisfiable("(?=abc)ab").isTrue();
-    assertSatisfiable("(?!abc)ab").isTrue();
   }
 
   @Test
-  void testSimpleUnsat() {
+  void testPositiveLookaheadSat() {
+    assertSatisfiable("(?=a)a").isTrue();
+    assertSatisfiable("(?=a)..").isTrue();
+    assertSatisfiable("(?=a)ab").isTrue();
+    assertSatisfiable("(?=a)(?=.b)ab").isTrue();
+    assertSatisfiable("(?=a)a(?=b)b").isTrue();
+    assertSatisfiable("(?=abc)ab(?=cd)cde").isTrue();
+    assertSatisfiable("(?:a(?=c)|b(?=d))c").isTrue();
+    assertSatisfiable("a(?=b)").isTrue();
+    assertSatisfiable("(?:-(?:one|[0-9]+([a-z](?=[^a-z]|$)|st|nd|rd|th)?))*").isTrue();
+//    assertSatisfiable("(..(?=ab))*").isTrue();  -> Need support for loop unrolling
+//    assertSatisfiable("(?=abc)ab").isTrue();  -> Need support for full/partial matching
+  }
+
+  @Test
+  void testPositiveLookaheadUnsat() {
     assertSatisfiable("(?=a)b").isFalse();
     assertSatisfiable("(?=ac)ab").isFalse();
     assertSatisfiable("(?=a)bc").isFalse();
-    assertSatisfiable("(?!a)a").isFalse();
-    assertSatisfiable("(?!ab)ab").isFalse();
+    assertSatisfiable("(?=a)(?=b).").isFalse();
+    assertSatisfiable("(?=abcd)ab(?=.e)..").isFalse();
+    assertSatisfiable("(?=[ab])(?=[bc])[ac]").isFalse();
     assertSatisfiable("(?=a)[^ba]").isFalse();
+  }
+
+  @Test
+  void testNegativeLookaheadSat() {
+    assertSatisfiable("(?!ab)..").isTrue();
+//    assertSatisfiable("a(?!:abc):ab").isTrue();  -> Need support for full/partial matching
+//    assertSatisfiable("(?!abc)ab").isTrue();  -> Need support for full/partial matching
+
+  }
+
+  @Test
+  void testNegativeLookaheadUnsat() {
+    assertSatisfiable("(?!a)a").isFalse();
+    assertSatisfiable("(?!a|b)a").isFalse();
+    assertSatisfiable("(?!ab)ab").isFalse();
+    assertSatisfiable("(?!.).").isFalse();
     assertSatisfiable("(?!.)ab").isFalse();
+//    assertSatisfiable("(?:a(?!bc))+bc").isFalse();  -> Need support for loop unrolling
+//    assertSatisfiable("(?:(x|y)(?!bc))+bc").isFalse();  -> Need support for loop unrolling
+  }
+
+  @Test
+  void testPositiveLookbehindSat() {
+    assertSatisfiable("(?<=a)b").isTrue();
+    assertSatisfiable("a(?<=a)").isTrue();
+    assertSatisfiable("abc(?<=bc)").isTrue();
+    assertSatisfiable("abc(?<=bc)d(?<=cd)e").isTrue();
+    assertSatisfiable("a(bc)*(?<=b|c)(?<=.)").isTrue();
+  }
+
+  @Test
+  void testPositiveLookbehindUnsat() {
+    assertSatisfiable("b(?<=a)").isFalse();
+    assertSatisfiable("abc(?<=c.)").isFalse();
+    assertSatisfiable("ab(?<=b)(?<=d.)").isFalse();
+    assertSatisfiable("abc(?<=bc)d(?<=bd)e").isFalse();
+    assertSatisfiable("[12](?<=[23])(?<=[13])").isFalse();
+  }
+
+  @Test
+  void testRepetitionSat() {
+//    assertSatisfiable("[12]*(?<=[23])(?<=[13])").isFalse();  -> Timeout
+//    assertSatisfiable("a(bc)*(?<!b|c)").isFalse(); -> Timeout
+//    assertSatisfiable("(?:a(?!bc)|d)+bc").isTrue();
+//    assertSatisfiable("(?:a((?!bc)|d)*)+bc").isTrue();
+  }
+
+  @Test
+  void testRepetitionUnsat() {
     assertSatisfiable("(?:a(?!bc))+bc").isFalse();
-    assertSatisfiable("(?:(x|y)(?!bc))+bc").isFalse();
+  }
+
+  @Test
+  void testNegativeLookbehindSat() {
+    assertSatisfiable("b(?<!a)").isTrue();
+    assertSatisfiable("abc(?<!c.)").isTrue();
+    assertSatisfiable("abc(?<!b)(?<!a\\d)").isTrue();
+    assertSatisfiable("ab.(?<!bc)d(?<!cd)e").isTrue();
+    assertSatisfiable("\\d(?<![025-9])(?<![13])").isTrue();
+  }
+
+  @Test
+  void testNegativeLookbehindUnsat() {
+    assertSatisfiable("a(?<!a)").isFalse();
+    assertSatisfiable("a(?<!.)").isFalse();
+    assertSatisfiable("abc(?<!bc)").isFalse();
+    assertSatisfiable("abc(?<!bc)d(?<!cd)e").isFalse();
+    assertSatisfiable("abc(?<!b|c)").isFalse();
+    assertSatisfiable("[12](?<![^23])(?<![^13])").isFalse();
+    assertSatisfiable("[0-9](?<![0245-9])(?<![13])").isFalse();
+  }
+
+  @Test
+  void testCombined() {
   }
 }
